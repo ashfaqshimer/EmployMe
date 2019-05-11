f52597507468ff797061a32d8ef1ce1251886435
Ashfaq <shimer.ahamed@gmail.com>
2019-05-12 01:01:02 +0530

    Create 404 page, setup views

--- app.js
+++ app.js
@@ -3,8 +3,14 @@ const bodyParser = require('body-parser');
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
+app.set('view engine', 'ejs');
 
 //Main code starts here
+
+//set the page not found
+app.use((req, res, next) => {
+    res.status(404).render('404', { pageTitle: 'Page Not Found' });
+});
 
 app.listen(3030, () => {
     console.log('App listening on port 3030!');
