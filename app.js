4da99c5ad89601eda19fc055b22a6291d6767683
Ashfaq <shimer.ahamed@gmail.com>
2019-05-12 19:48:45 +0530

    Create landing page, with navbar and footer

--- app.js
+++ app.js
@@ -3,9 +3,13 @@ const bodyParser = require('body-parser');
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
 //Main code starts here
+app.get('/', (req, res) => {
+    res.render('home', {pageTitle:'EmployMe', path:'/'});
+});
 
 //set the page not found
 app.use((req, res, next) => {
