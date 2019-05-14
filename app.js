46836b663232c9328230e5159048d1a304777742
Ashfaq <shimer.ahamed@gmail.com>
2019-05-14 11:13:46 +0530

    Create signup form, login forms, navbar for job seeker

--- app.js
+++ app.js
@@ -15,10 +15,19 @@ app.get('/signup', (req, res) => {
     res.render('signup', { pageTitle: 'Sign Up', path: '/signup' });
 });
 
+app.get('/login/jobseeker', (req, res) => {
+    res.render('login-jobSeeker', { pageTitle: 'Job-Seeker Login', path: '/login' });
+});
+
+app.get('/login/admin', (req, res) => {
+    res.render('login-admin', { pageTitle: 'Admin Login', path: '/login' });
+});
 //set the page not found
 app.use((req, res, next) => {
     res.status(404).render('404', { pageTitle: 'Page Not Found' });
 });
+
+
 
 app.listen(3030, () => {
     console.log('App listening on port 3030!');
