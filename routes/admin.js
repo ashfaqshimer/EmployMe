7308cfb9c88482120741ea93adcbfa1ca00462ca
Ashfaq <shimer.ahamed@gmail.com>
2019-05-20 21:58:22 +0530

    Create views

--- routes/admin.js
+++ routes/admin.js
@@ -2,4 +2,8 @@ const express = require('express');
 
 const router = express.Router();
 
+router.get('/', (req, res) => {
+    res.render('admin/home', { pageTitle: 'Welcome Administrator', path: '/admin/' });
+});
+
 module.exports = router;
