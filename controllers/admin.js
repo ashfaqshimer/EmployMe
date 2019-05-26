0d62f86f5dabe9b2913e67967a9dc3ed9e0e4026
Ashfaq <shimer.ahamed@gmail.com>
2019-05-26 17:57:39 +0530

    Complete admin search form

--- controllers/admin.js
+++ controllers/admin.js
@@ -2,6 +2,20 @@ const User = require('../models/user');
 const Resume = require('../models/resume');
 const Lookup = require('../models/lookup');
 
+const getValues = require('../getValues');
+
+exports.getHome = (req, res) => {
+    res.render('admin/search-candidate', { pageTitle: 'Administrator', path: '/admin/', 
+    phd            : getValues.phd,
+    professional   : getValues.professional,
+    diploma        : getValues.diploma,
+    masters        : getValues.masters,
+    bachelors      : getValues.bachelors,
+    jobSector      : getValues.jobSector,
+    educationLevel : getValues.educationLevel
+  });
+}
+
 exports.postSearch = (req,res)=>{
     console.log(req.body)
 }
--- routes/admin.js
+++ routes/admin.js
@@ -4,9 +4,8 @@ const adminControllers = require('../controllers/admin')
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-	res.render('admin/search-candidate', { pageTitle: 'Administrator', path: '/admin/' });
-});
+router.get('/', adminControllers.getHome);
 
 router.post('/search', adminControllers.postSearch)
+
 module.exports = router;
