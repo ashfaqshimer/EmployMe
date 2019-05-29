c86c623728b8952cf9442982f3d6ea8bdc3aa9e7
Ashfaq Shimer <shimer.ahamed@gmail.com>
2019-05-29 23:30:59 +0530

    enable authentication in admin route

--- routes/admin.js
+++ routes/admin.js
@@ -1,19 +1,19 @@
-const express = require("express");
+const express = require('express');
 
-const adminControllers = require("../controllers/admin");
-const isAuth = require("../middleware/isAuthAdmin");
+const adminControllers = require('../controllers/admin');
+const isAuth = require('../middleware/isAuthAdmin');
 
 const router = express.Router();
 
 // Authentication has been commented out. Re-enable after development
 
-// router.get('/', isAuth, adminControllers.getHome);
-router.get("/", adminControllers.getHome);
+router.get('/', isAuth, adminControllers.getHome);
+// router.get("/", adminControllers.getHome);
 
-// router.post('/searchsector', isAuth, adminControllers.postSearchSector);
-router.post("/searchsector", adminControllers.postSearchSector);
+router.post('/searchsector', isAuth, adminControllers.postSearchSector);
+// router.post("/searchsector", adminControllers.postSearchSector);
 
-// router.post('/search', isAuth, adminControllers.postSearch);
-router.post("/search", adminControllers.postSearch);
+router.post('/search', isAuth, adminControllers.postSearch);
+// router.post("/search", adminControllers.postSearch);
 
 module.exports = router;
